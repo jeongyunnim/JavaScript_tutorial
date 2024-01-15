1352e755c6b73e5708518ad79625d387e3b6c856
jeongyunnim <jeongyun7521@gmail.com>
2024-01-15 20:39:05 +0900

    [Feat] prompt를 통해 사용자 입력으로 maxHP를 정의

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -3,12 +3,26 @@ const STRONG_ATTACK_VALUE = 17
 const MONSTER_VALUE = 14
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const enteredValue = prompt('Maximum life for you and the monster.', '100');
+let chosenMaxLife = parseInt(enteredValue);
+
+if (isNaN(chosenMaxLife) === true || chosenMaxLife <= 0)
+{
+    chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
+
+function reset()
+{
+    resetGame(chosenMaxLife);
+    currentMonsterHealth = chosenMaxLife;
+    currentPlayerHealth = chosenMaxLife;
+}
 
 function endRound()
 {
@@ -35,6 +49,10 @@ function endRound()
     else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0)
     {
         alert("You have a draw!");
+    }
+    if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0)
+    {
+        reset();
     }
 }
 
