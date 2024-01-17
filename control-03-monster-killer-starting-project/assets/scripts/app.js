ccfcc5fa3c110f2faced8007050209cac698d012
jeongyunnim <jeongyun7521@gmail.com>
2024-01-18 00:34:46 +0900

    [Feat] 사용자 에러 추가 예제 완료

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -11,16 +11,30 @@ const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
 const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
 const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 
-const enteredValue = prompt('Maximum life for you and the monster.', '100');
-
-let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
 
-if (isNaN(chosenMaxLife) === true || chosenMaxLife <= 0)
+function getMaxLifeValues()
 {
-    chosenMaxLife = 100;
+    const enteredValue = prompt('Maximum life for you and the monster.', '100');
+    const parsedValue = parseInt(enteredValue);
+    if (isNaN(parsedValue) === true || parsedValue <= 0)
+    {
+        throw { message: 'Invalid user input, not a number!'};
+    }
+    return parsedValue;
 }
 
+let chosenMaxLife;
+
+try 
+{
+    chosenMaxLife = getMaxLifeValues();
+}
+catch (error)
+{
+    console.log(error);
+    chosenMaxLife = 100;
+}
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
