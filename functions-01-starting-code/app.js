576807fde65a8cd392beaece75f905fc2f7a88f7
jeongyunnim <jeongyun7521@gmail.com>
2024-01-18 12:36:11 +0900

    [Feat] 함수 객체 변수에 저장하여 호출

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,1 +1,8 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame()
+{
+	console.log("Game is starting...");
+}
+
+startGameBtn.addEventListener('click', startGame);
