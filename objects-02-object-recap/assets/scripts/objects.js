df569f40d1db419f79a1be76a80b6fe6f6d62a50
jeongyunnim <jeongyun7521@gmail.com>
2024-01-31 20:44:35 +0900

    [Feat] 동적 프로퍼티 받아서 내용 출력
    동적으로 입력 받은 프로퍼티를 반복문을 이용하여 출력하는 부분까지 완성

---
+++ objects-02-object-recap/assets/scripts/objects.js
@@ -0,0 +1,56 @@
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getAnimations('search-btn');
+
+const movies = [];
+
+const renderMovies = () => {
+  const movieList = document.getElementById('movie-list');
+
+  if (movies.length === 0)
+  {
+    movieList.classList.remove('visible');
+  }
+  else
+  {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
+
+  movies.forEach((movie) => {
+    const movieElement = document.createElement('li');
+    let text = movie.info.title + ' - ';
+    for (const key in movie.info)
+    {
+      if (key !== 'title')
+      {
+        text = text + `${key}: ${movie.info[key]}`;
+      }
+    }
+    movieElement.textContent = text;
+    movieList.append(movieElement);
+  });
+}
+
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
+
+  if (title.trim() === '' || extraName === '' || extraValue === '')
+  {
+    return ;
+  }
+  
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue
+    },
+    id: Math.random()
+  };
+
+  movies.push(newMovie);
+  renderMovies();
+}
+
+addMovieBtn.addEventListener('click', addMovieHandler);
