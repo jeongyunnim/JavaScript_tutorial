1b53d25040717f660af27afc2b6b125adeef2251
jeongyunnim <jeongyun7521@gmail.com>
2024-02-08 21:11:45 +0900

    [Feat] HTTP promise화 완료

---
+++ Practice-HTTP/assets/scripts/app.js
@@ -0,0 +1,51 @@
+const listElement = document.querySelector('.posts');
+const postsTemplate = document.getElementById('single-post');
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector('#available-posts button')
+
+function sendHttpRequest(method, url, data = null) {
+	const promise = new Promise((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+		
+		xhr.open(method, url);
+		xhr.responseType = 'json';
+		xhr.onload = function() {
+			resolve(xhr.response);
+		}
+		
+		xhr.send(JSON.stringify(data));
+	});
+	return promise;
+}
+
+async function fetchPosts() {
+	const reponseData = await sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts');
+	for (const post of reponseData)
+	{
+		const postEl = document.importNode(postsTemplate.content, true);
+		postEl.querySelector('h2').textContent = post.title.toUpperCase();
+		postEl.querySelector('p').textContent = post.body;
+		listElement.append(postEl);
+	};
+}
+
+async function createPost(title, content) {
+	const postId = Math.random();
+	const post = {
+		title: title,
+		body: content,
+		userId: postId
+	};
+	sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post)
+}
+
+
+fetchButton.addEventListener('click', fetchPosts);
+form.addEventListener('submit', event => {
+	event.preventDefault();
+	const enteredTitle = event.currentTarget.querySelector('#title').value;
+	const enteredContent = event.currentTarget.querySelector('#content').value;
+	//유효성 검사
+
+	createPost(enteredTitle, enteredContent);
+})
